a7f3ae01384aa5bf1f1fce447503b0e98484c074
mfalbarifq1 <mfalbarif.q1@gmail.com>
2026-07-14 10:33:04 +0700

    main program

--- function_no_parameter.cpp
+++ function_no_parameter.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-float CekStatusBerparameter(float x, float y){
+float HitungRerataBerparameter(float x, float y){
     return(x + y) / 2 ;
 }
 string CekStatusBerparameter(float a){
@@ -11,4 +11,16 @@ string CekStatusBerparameter(float a){
     else{
         return"tidaklulus";
     }
+}
+
+int main(){
+    float ujian1,ujian2;
+    cout<<"masukkan nilai ujian 1 :";
+    cin>>ujian1;
+    cout<<"masukkan nilai ujian 2 :";
+    cin>>ujian2;
+
+    cout<<"status kelulusan:"<<CekStatusBerparameter(HitungRerataBerparameter(ujian1, ujian2));
+    
+
 }
