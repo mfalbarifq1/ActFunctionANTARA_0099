95ddaec72ef2da94901dd2ea37f9d539ec6b39eb
mfalbarifq1 <mfalbarif.q1@gmail.com>
2026-07-14 10:53:39 +0700

    main program

--- function_with_parameter.cpp
+++ function_with_parameter.cpp
@@ -1,2 +1,28 @@
 #include<iostream>
 using namespace std;
+
+int panjang,lebar;
+void input(){
+    cout<<"masukkan panjang";
+    cin>>panjang;
+    cout<<"masukkan lebar";
+    cin>>lebar;
+
+}
+int luaspersegi(int a,int b){
+    return a*b;
+
+}
+int kelilingpersegi(int a, int b){
+    return 2*(a+b);
+
+}
+void output(){
+    cout<<"luasnya: "<<luaspersegi(panjang,lebar)<<endl;
+}
+
+int main(){
+    input();
+    output();
+    cout<<"kelilingnya:" <<kelilingpersegi(panjang, lebar );
+}
