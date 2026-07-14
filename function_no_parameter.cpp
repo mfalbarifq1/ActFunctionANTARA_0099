4d3a930e2ed3aae475254807ad746399e623339c
mfalbarifq1 <mfalbarif.q1@gmail.com>
2026-07-14 10:28:54 +0700

    fungsi cekstatus beda parameter

--- function_no_parameter.cpp
+++ function_no_parameter.cpp
@@ -4,3 +4,11 @@ using namespace std;
 float CekStatusBerparameter(float x, float y){
     return(x + y) / 2 ;
 }
+string CekStatusBerparameter(float a){
+    if(a >= 60){
+        return "lulus";
+    }
+    else{
+        return"tidaklulus";
+    }
+}
